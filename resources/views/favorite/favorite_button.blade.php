ec80ea54554dfbd989c613cd9c4b4e5585e2c6aa
chika.masuda <c.m.062820@gmail.com>
2020-07-01 15:37:30 +0000

    chenge unfavorite-button-color

--- resources/views/favorite/favorite_button.blade.php
+++ resources/views/favorite/favorite_button.blade.php
@@ -1,6 +1,6 @@
 @if (Auth::user()->is_favorite($micropost->id))
         {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-sm mr-3"]) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-primary btn-sm mr-3"]) !!}
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
